e8190bd5df46e7ca365c4a5e87be5e86ee031d78
Josh Nothum <joshnothum@JN.local>
2017-10-27 15:46:19 -0500

    server.js added; and bootstrap

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var port = process.env.PORT || 5000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.listen(port, function () {
+
+    console.log('thx for listening on port:', port);
+    
+});
