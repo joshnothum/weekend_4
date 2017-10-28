b621465f91b19ef4b3878c65483330c5560029dd
Josh Nothum <joshnothum@JN.local>
2017-10-28 12:47:21 -0500

    first commit second day

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,10 @@
-console.log('we sourced');
+console.log('js soruced');
+
+var galleryApp = angular.module('galleryApp', []);
+
+galleryApp.controller('GalleryController', function ($http) {
+    var gallery = this;
+    var galleryOfWeirdos = [];
+    
+
+});
