ed6aa447d8b8eba0184ea971b55efb42961c6b75
Josh Nothum <joshnothum@JN.local>
2017-10-28 20:47:05 -0500

    i messed my css up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('GalleryController', function () {
     var gallery = this;
-    gallery.pictures = [{name:'shower'}, {name:'manuel'}, {name: 'new_bpg'}];
-    
+    gallery.pictures = [{name:'shower', description:'Se fue el agua' }, {name:'manuel'}, {name: 'new_bpg_opt'}];
+    gallery.count = 0;
+    gallery.addButton = function () {
+        gallery.count += 1;
+    };
     
 });
